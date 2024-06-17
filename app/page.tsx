4987026ac5de1daaa761f2ec883340d54e469e51
vberkoz <vberkoz@gmail.com>
2024-06-17 12:03:16 +0300

    added per-user data isolation

--- app/page.tsx
+++ app/page.tsx
@@ -42,6 +42,7 @@ export default function App() {
     <Authenticator>
       {({ signOut, user }) => (
         <main>
+          <h1>{user?.signInDetails?.loginId}'s todos</h1>
           <h1>My todos</h1>
           <button onClick={createTodo}>+ new</button>
           <ul>
